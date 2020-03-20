0a490dfe9f8a85e5c57baa426cec64f31722fb9d
seriousdave42 <seriousdave42@gmail.com>
2020-03-19 23:34:40 -0500

    build commit

---
+++ client/build/precache-manifest.e4f538e05113bf12da50a81c4970ff59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d45f9c350fe329961e9a72292365bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1060c798cb798d66df96",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f5780d38fbed0eb4744",
+    "url": "/static/js/2.0b091e91.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0b091e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1060c798cb798d66df96",
+    "url": "/static/js/main.f19ee0e5.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
